bb664251d1a8239661695a855e2ced0ff0233746
Rizal Fahmi <rizalfahmi3002@gmail.com>
2024-10-04 17:49:34 +0700

    Change spasi format

--- src/components/content/FormatPopulation.jsx
+++ src/components/content/FormatPopulation.jsx
@@ -1,10 +1,10 @@
 const formatPopulation = (population) => {
   if (population >= 1e9) {
-    return (population / 1e9).toFixed(2) + 'B';
+    return (population / 1e9).toFixed(2) + ' B';
   } else if (population >= 1e6) {
-    return (population / 1e6).toFixed(2) + 'M';
+    return (population / 1e6).toFixed(2) + ' M';
   } else if (population >= 1e3) {
-    return (population / 1e3).toFixed(2) + 'K';
+    return (population / 1e3).toFixed(2) + ' K';
   } else {
     return population.toString();
   }
