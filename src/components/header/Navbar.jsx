55724d97adead9b07847aa63a2ae61728cd0828a
Rizal Fahmi <rizalfahmi3002@gmail.com>
2024-10-04 07:42:13 +0700

    Change logo

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -10,13 +10,10 @@ function Navbar() {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="logo.png"
               className="h-8"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white text-gray-600">
-              NationStats
-            </span>
           </Link>
           <button
             data-collapse-toggle="navbar-default"
