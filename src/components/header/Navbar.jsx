e7b356be06309364e6374dcdbb067af0f3fc027d
Rizal Fahmi <rizalfahmi3002@gmail.com>
2024-10-09 23:56:21 +0700

    memperbaiki responsif saat mode mobile dan merapikan tampilan

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,23 +1,27 @@
 import { Link, NavLink, Outlet } from 'react-router-dom';
-import logo from '../../../public/logo.png';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <nav className="bg-white dark:bg-gray-900 border-b border-gray-400">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="bg-white dark:bg-gray-900 border-b-2 border-gray-300">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-3">
           <Link
             to={`/`}
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img src={logo} className="h-8" alt="nationstats logo" />
+            <img src="/logo.png" className="h-8" alt="nationstats logo" />
           </Link>
           <button
-            data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
+            onClick={toggleMenu}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-blue-400 rounded-lg md:hidden hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -36,15 +40,22 @@ function Navbar() {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div
+            className={`w-full md:block md:w-auto ${
+              isOpen ? 'block' : 'hidden'
+            }`}
+            id="navbar-default"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-4 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to={`/`}
                   className={({ isActive }) =>
-                    `block py-2 px-3 mx-2 font-medium rounded ${
-                      isActive ? 'text-blue-700' : 'text-gray-700'
-                    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white`
+                    `block w-full py-2 px-3 font-medium rounded-md mt-2 transition duration-300 ease-in-out ${
+                      isActive
+                        ? 'bg-blue-400 text-white'
+                        : 'text-gray-700 hover:bg-blue-400 hover:text-white'
+                    } md:w-auto md:hover:bg-blue-400 md:hover:text-white dark:text-white`
                   }
                 >
                   Home
@@ -54,9 +65,11 @@ function Navbar() {
                 <NavLink
                   to={`/compare`}
                   className={({ isActive }) =>
-                    `block py-2 px-3 mx-2 font-medium rounded ${
-                      isActive ? 'text-blue-700' : 'text-gray-700'
-                    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white`
+                    `block w-full py-2 px-3 font-medium rounded-md mt-2 transition duration-300 ease-in-out ${
+                      isActive
+                        ? 'bg-blue-400 text-white'
+                        : 'text-gray-700 hover:bg-blue-400 hover:text-white'
+                    } md:w-auto md:hover:bg-blue-400 md:hover:text-white dark:text-white`
                   }
                 >
                   Compare
@@ -66,9 +79,11 @@ function Navbar() {
                 <NavLink
                   to={`/news`}
                   className={({ isActive }) =>
-                    `block py-2 px-3 mx-2 font-medium rounded ${
-                      isActive ? 'text-blue-700' : 'text-gray-700'
-                    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white`
+                    `block w-full py-2 px-3 font-medium rounded-md mt-2 transition duration-300 ease-in-out ${
+                      isActive
+                        ? 'bg-blue-400 text-white'
+                        : 'text-gray-700 hover:bg-blue-400 hover:text-white'
+                    } md:w-auto md:hover:bg-blue-400 md:hover:text-white dark:text-white`
                   }
                 >
                   News
